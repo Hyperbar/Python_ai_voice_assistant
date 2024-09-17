568e369e33f8d1ff0a4b799cbfe4c6e71377021f
hyperbar <szthyperbar@gmail.com>
2024-09-17 17:07:29 +0200

    Initial commit

---
+++ README.txt
@@ -0,0 +1,10 @@
+AI-Voice-Assitant
+
+with Voice activity detection and variable files
+
+using Openai
+using livekit server https://livekit.io/
+using Dotenv
+using Silero
+
+keys in .env have been deleted
---
+++ main.py
@@ -0,0 +1,19 @@
+import asyncio
+from dotenv import load_dotenv
+
+from livekit.agents import AutoSubscribe, JobContext, WorkerOptions, cli, llm
+from livekit.agents.voice_assistant import VoiceAssistant
+from livekit.plugins import openai, silero
+
+load_dotenv()
+
+async def entrypoint(ctx: JobContext):
+    initial_ctx = llm.ChatContext().append(
+        role="system",
+        text=(
+            "You are a voice assistant created by LiveKit"
+        )
+    )
+
+if __name__ == "__main__":
+    cli.run_app(WorkerOptions(entrypoint_fnc=entrypoint))
